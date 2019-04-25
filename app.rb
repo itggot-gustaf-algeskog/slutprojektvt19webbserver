810d705c116626260be56d96af6f16e224414ef6
itggot-gustaf-algeskog <35598573+itggot-gustaf-algeskog@users.noreply.github.com>
2019-04-25 10:54:37 +0200

    skapade startsidan som läser in produkter

--- app.rb
+++ app.rb
@@ -4,3 +4,11 @@ require 'sqlite3'
 require 'bcrypt'
 enable :sessions
 require_relative 'functions.rb'
+
+before('/') do
+    get_products
+end
+
+get('/') do
+    slim(:index, locals:{ products: session[:products]})
+end
