c3eff2031511c8533bcaab62d12bba087b123a76
itggot-gustaf-algeskog <35598573+itggot-gustaf-algeskog@users.noreply.github.com>
2019-05-01 16:11:58 +0200

    la till så att de nya sidorna hämtas och gjorde post funktioner

--- app.rb
+++ app.rb
@@ -12,3 +12,31 @@ end
 get('/') do
     slim(:index, locals:{ products: session[:products]})
 end
+
+get('/login') do
+    slim(:login)
+end
+
+get('/register') do
+    slim(:registrera)
+end
+
+before('/logga_in') do
+    inloggning
+end
+
+post('/logga_in') do
+    if session[:username] == session[:info][0][0] and BCrypt::Password.new(session[:info][0][1]) == session[:password]
+        redirect('/')
+    else
+        redirect('/no_access')
+    end
+end
+
+before('/registrering') do
+    registrering
+end
+
+post('/registrering') do    
+    redirect('/')
+end
