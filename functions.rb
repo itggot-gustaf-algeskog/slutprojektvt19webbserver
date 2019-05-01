6cff3705a917bdf97f846013f27378af7c0d5883
itggot-gustaf-algeskog <35598573+itggot-gustaf-algeskog@users.noreply.github.com>
2019-05-01 16:12:19 +0200

    gjorde funktionerna för inloggning och registrering

--- functions.rb
+++ functions.rb
@@ -4,3 +4,47 @@ def get_products
 
     session[:products] = db.execute("SELECT Name,Description,Rating,Price,Productid FROM products")
 end
+
+def inloggning
+    db = SQLite3::Database.new('db/shop.db')
+    db.results_as_hash = true
+
+    session[:username] = params["username"]
+    session[:password] = params["password"]
+
+    person = db.execute("SELECT (Id) FROM users WHERE Name = (?)",session[:username])
+    
+    if person[0] == nil
+        redirect('/no_access')
+    else
+        session[:id] = person[0][0]
+    end
+
+    session[:info] = db.execute("SELECT * FROM users WHERE Id = (?)",session[:id])
+end
+
+def registrering
+    db = SQLite3::Database.new('db/shop.db')
+    db.results_as_hash = true
+
+    session[:new_username] = params["new_username"]
+    session[:new_password] = params["new_password"]
+    session[:confirm_password] = params["confirm_password"]
+
+    existing_username = db.execute("SELECT Name FROM users WHERE Name = (?)",session[:new_username])
+    if existing_username == []
+        existing_username = ""
+    else
+        existing_username = existing_username[0][0]
+    end
+
+    if session[:new_username] == ""
+    elsif existing_username == session[:new_username]
+    elsif session[:new_password] == ""
+    elsif session[:new_password] != session[:confirm_password]
+    else
+        hashat_password = BCrypt::Password.create(session[:new_password])
+        db.execute("INSERT INTO users (Name,Secret) VALUES (?,?)",session[:new_username],hashat_password)
+        db.execute("INSERT INTO profiles (Profiledescription,Item,Interest,Username) VALUES (?,?,?,?)","hej","hej","hej",session[:new_username])
+    end
+end
