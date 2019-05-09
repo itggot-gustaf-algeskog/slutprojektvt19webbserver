5b633040c1effb5feb5ac12dc4a3d0b9c1bd584a
itggot-gustaf-algeskog <35598573+itggot-gustaf-algeskog@users.noreply.github.com>
2019-05-09 10:56:49 +0200

    dokumentation

--- app.rb
+++ app.rb
@@ -4,20 +4,27 @@ require 'sqlite3'
 require 'bcrypt'
 enable :sessions
 require_relative 'functions.rb'
+include Functions
 
 before('/') do
     get_products
     user_info
 end
 
+# Display Landing Page
+#
 get('/') do
     slim(:index, locals:{ products: session[:products], user_info: session[:user_info]})
 end
 
+# Display a Login form
+#
 get('/login') do
     slim(:login)
 end
 
+# Display a Register form
+#
 get('/register') do
     slim(:registrera)
 end
@@ -26,6 +33,8 @@ before('/logga_in') do
     inloggning
 end
 
+# Attempts login and updates the session
+#
 post('/logga_in') do
     if session[:username] == session[:info][0][0] and BCrypt::Password.new(session[:info][0][1]) == session[:password]
         redirect('/')
@@ -41,6 +50,8 @@ before('/registrering') do
     registrering
 end
 
+# Attempts register and creates a user
+#
 post('/registrering') do    
     if session[:error] != nil
         redirect('/error')
@@ -55,6 +66,8 @@ before('/produktsida/:id') do
     get_comments
 end
 
+# Displays a single Product
+#
 get('/produktsida/:id') do
     slim(:produktsida, locals:{ produktsida: session[:produktsida], comments: session[:comments]})
 end
@@ -63,10 +76,14 @@ before('/search') do
     search
 end
 
+# Attempts finding products and redirects to '/search_result'
+#
 post('/search') do
     redirect('/search_result')
 end
 
+# Displays search results
+#
 get('/search_result') do
     slim(:search_result, locals:{ results: session[:results]})
 end
@@ -77,6 +94,8 @@ before('/thumbs_up') do
     end
 end
 
+# Updates an existing rating and redirects back
+#
 post('/thumbs_up') do
     if session[:id] != nil
         redirect back
@@ -91,6 +110,8 @@ before('/thumbs_down') do
     end
 end
 
+# Updates an existing rating and redirects back
+#
 post('/thumbs_down') do
     if session[:id] != nil
         redirect back
@@ -99,6 +120,8 @@ post('/thumbs_down') do
     end
 end
 
+# Displays an error message
+#
 get('/rating_error') do
     slim(:rating_error)
 end
@@ -107,10 +130,14 @@ before('/profile') do
     profiles
 end
 
+# Displays a user's profile
+#
 get('/profile') do
     slim(:profile, locals:{ profiles: session[:profiles]})
 end
 
+# Updates session
+#
 post('/logout') do
     session[:username] = nil
     session[:password] = nil
@@ -118,10 +145,14 @@ post('/logout') do
     redirect('/')
 end
 
+# Displays an error message
+#
 get('/no_access') do
     slim(:no_access)
 end
 
+# Displays an error message
+#
 get('/error') do
     slim(:error)
 end
@@ -130,19 +161,27 @@ before('/create_comment') do
     create_comment
 end
 
+# Creates a new comment and redirects back
+#
 post('/create_comment') do
     if session[:create_comment_error] == 1
         session[:create_comment_error] = nil
         redirect('/create_comment_error')
+    elsif session[:id] == nil
+        redirect('/login')
     else
         redirect back
     end
 end
 
+# Displays an error message
+#
 get('/create_comment_error') do
     slim(:create_comment_error)
 end
 
+# Displays an update form for a singel comment
+#
 get('/edit_comment/:id') do
     session[:comment_id] = params["id"]
 
@@ -153,6 +192,8 @@ before('/redigera') do
     update_comment
 end
 
+# Updates an existing comment and redirects to '/'
+#
 post('/redigera') do
     if session[:update_comment_error] == 1
         session[:update_comment_error] = nil
@@ -162,6 +203,8 @@ post('/redigera') do
     end
 end
 
+# Displays an error message
+#
 get('/update_comment_error') do
     slim(:update_comment_error)
 end
@@ -170,10 +213,14 @@ before('/delete_comment') do
     delete_comment
 end
 
+# Deletes an existing comment and redirects to '/'
+#
 post('/delete_comment') do
     redirect('/')
 end
 
+# Displays an update form for a singel profile
+#
 get('/edit_profile') do
     slim(:edit_profile)
 end
@@ -182,6 +229,8 @@ before('/update_profile') do
     update_profile
 end
 
+# Updates an existing profile and redirects to '/articles'
+#
 post('/update_profile') do
     redirect('/profile')
 end
@@ -190,6 +239,8 @@ before('/add_to_cart') do
     add_to_cart
 end
 
+# Creates a product display in '/kundvagn'
+#
 post('/add_to_cart') do
     if session[:add_to_cart_error] == 1
         session[:add_to_cart_error] = nil
@@ -199,6 +250,8 @@ post('/add_to_cart') do
     end
 end
 
+# Displays an error message
+#
 get('/add_to_cart_error') do
     slim(:add_to_cart_error)
 end
@@ -207,10 +260,14 @@ before('/kundvagn/:id') do
     get_kundvagn
 end
 
+# Displays a singel user's cart
+#
 get('/kundvagn/:id') do
     slim(:kundvagn, locals:{ items: session[:items]})
 end
 
+# Displays cart for non logged in users
+#
 get('/kundvagn') do
     slim(:kundvagn)
 end
@@ -219,17 +276,14 @@ before('/buy_products') do
     buy_products
 end
 
+# Deletes products from a singel user's cart
+#
 post('/buy_products') do
     redirect back
 end
 
-before('/delete_product') do
-end
-
-post('/delete_product') do
-    redirect back
-end
-
+# Displays a confirmation message when deleting a singel product from a singel user's cart
+#
 get('/edit_order/:id') do
     session[:itemid] = params["id"]
 
@@ -240,6 +294,8 @@ before('/delete_item') do
     delete_item
 end
 
+# Deletes a singel product from a singel user's cart
+#
 post('/delete_item') do
     redirect('/')
 end
--- functions.rb
+++ functions.rb
@@ -1,195 +1,196 @@
-def get_products
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
+module Functions
+    def get_products
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
 
-    session[:products] = db.execute("SELECT Name,Description,Rating,Price,Productid FROM products")
-end
-
-def user_info
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
-
-    session[:user_info] = db.execute("SELECT Id FROM users WHERE Name = (?)",session[:username])
-end
-
-def inloggning
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
-
-    session[:username] = params["username"]
-    session[:password] = params["password"]
-
-    person = db.execute("SELECT (Id) FROM users WHERE Name = (?)",session[:username])
-    
-    if person[0] == nil
-        redirect('/no_access')
-    else
-        session[:id] = person[0][0]
+        session[:products] = db.execute("SELECT Name,Description,Rating,Price,Productid FROM products")
     end
 
-    session[:info] = db.execute("SELECT * FROM users WHERE Id = (?)",session[:id])
-end
+    def user_info
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
 
-def registrering
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
-
-    session[:new_username] = params["new_username"]
-    session[:new_password] = params["new_password"]
-    session[:confirm_password] = params["confirm_password"]
-
-    existing_username = db.execute("SELECT Name FROM users WHERE Name = (?)",session[:new_username])
-    if existing_username == []
-        existing_username = ""
-    else
-        existing_username = existing_username[0][0]
+        session[:user_info] = db.execute("SELECT Id FROM users WHERE Name = (?)",session[:username])
     end
 
-    if session[:new_username] == ""
-        session[:error] = 1
-    elsif existing_username == session[:new_username]
-        session[:error] = 2
-    elsif session[:new_password] == ""
-        session[:error] = 3
-    elsif session[:new_password] != session[:confirm_password]
-        session[:error] = 4
-    else
-        session[:error] = nil
-        hashat_password = BCrypt::Password.create(session[:new_password])
-        db.execute("INSERT INTO users (Name,Secret) VALUES (?,?)",session[:new_username],hashat_password)
-        db.execute("INSERT INTO profiles (Profiledescription,Item,Interest,Username) VALUES (?,?,?,?)","hej","hej","hej",session[:new_username])
+    def inloggning
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
+
+        session[:username] = params["username"]
+        session[:password] = params["password"]
+
+        person = db.execute("SELECT (Id) FROM users WHERE Name = (?)",session[:username])
+        
+        if person[0] == nil
+            
+        else
+            session[:id] = person[0][0]
+        end
+
+        session[:info] = db.execute("SELECT * FROM users WHERE Id = (?)",session[:id])
     end
-end
 
-def produktsida
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
+    def registrering
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
 
-    session[:produktsida] = db.execute("SELECT Name,Description,Rating,Price,Productid FROM products WHERE Productid = (?)",session[:produktid])
-end
+        session[:new_username] = params["new_username"]
+        session[:new_password] = params["new_password"]
+        session[:confirm_password] = params["confirm_password"]
 
-def search
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
+        existing_username = db.execute("SELECT Name FROM users WHERE Name = (?)",session[:new_username])
+        if existing_username == []
+            existing_username = ""
+        else
+            existing_username = existing_username[0][0]
+        end
 
-    session[:search] = params["search"].capitalize
-
-    session[:results] = db.execute("SELECT Name,Description,Rating,Price,Productid FROM products WHERE Name = (?)",session[:search])
-end
-
-def thumbs_up
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
-
-    rating = db.execute("SELECT Rating FROM products WHERE Productid = (?)",session[:produktid])
-    
-    new_rating = rating[0][0] + 1
-
-    db.execute("UPDATE products SET Rating = (?) WHERE Productid = (?)",new_rating,session[:produktid])
-end
-
-def thumbs_down
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
-
-    rating = db.execute("SELECT Rating FROM products WHERE Productid = (?)",session[:produktid])
-    
-    new_rating = rating[0][0] - 1
-
-    db.execute("UPDATE products SET Rating = (?) WHERE Productid = (?)",new_rating,session[:produktid])
-end
-
-def profiles
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
-
-    session[:profiles] = db.execute("SELECT Profiledescription,Item,Interest,Username FROM profiles WHERE Username = (?)",session[:username])
-end
-
-def create_comment
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
-
-    session[:comment] = params["comment"]
-
-    if session[:comment] == ""
-        session[:create_comment_error] = 1
-    elsif session[:id] == nil
-        redirect('/login')
-    else
-        db.execute("INSERT INTO comments (Comment,Commenter,Productid,Userid) VALUES (?,?,?,?)",session[:comment],session[:username],session[:produktid],session[:id])
+        if session[:new_username] == ""
+            session[:error] = 1
+        elsif existing_username == session[:new_username]
+            session[:error] = 2
+        elsif session[:new_password] == ""
+            session[:error] = 3
+        elsif session[:new_password] != session[:confirm_password]
+            session[:error] = 4
+        else
+            session[:error] = nil
+            hashat_password = BCrypt::Password.create(session[:new_password])
+            db.execute("INSERT INTO users (Name,Secret) VALUES (?,?)",session[:new_username],hashat_password)
+            db.execute("INSERT INTO profiles (Profiledescription,Item,Interest,Username) VALUES (?,?,?,?)","hej","hej","hej",session[:new_username])
+        end
     end
-end
 
-def get_comments
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
+    def produktsida
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
 
-    session[:comments] = db.execute("SELECT Comment,Commenter,Productid,Userid,Commentid FROM comments WHERE Productid = (?)", session[:produktid])
-end
-
-def update_comment
-    db = SQLite3::Database.new("db/shop.db")
-    db.results_as_hash = true
-
-    session[:update_comment] = params["update_comment"]
-
-    if session[:update_comment] == ""
-        session[:update_comment_error] = 1
-    else
-        db.execute("UPDATE comments SET Comment = (?) WHERE Commentid = (?)",session[:update_comment],session[:comment_id])
+        session[:produktsida] = db.execute("SELECT Name,Description,Rating,Price,Productid FROM products WHERE Productid = (?)",session[:produktid])
     end
-end
 
-def delete_comment
-    db = SQLite3::Database.new("db/shop.db")
-    db.results_as_hash = true
+    def search
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
 
-    db.execute("DELETE FROM comments WHERE Commentid = ?", session[:comment_id])
-end
+        session[:search] = params["search"].capitalize
 
-def update_profile
-    db = SQLite3::Database.new("db/shop.db")
-    db.results_as_hash = true
-
-    session[:profile_description] = params["profile_description"]
-    session[:item] = params["item"]
-    session[:interest] = params["interest"]
-
-    db.execute("UPDATE profiles SET Profiledescription = ?, Item = ?, Interest = ? WHERE Username = ?",session[:profile_description],session[:item],session[:interest],session[:username])
-end
-
-def add_to_cart
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
-
-    item_name = db.execute("SELECT Name FROM products WHERE Productid = (?)",session[:produktid])
-    item_price = db.execute("SELECT Price FROM products WHERE Productid = (?)",session[:produktid])
-
-    if session[:id] != nil
-        db.execute("INSERT INTO kundvagnar (Product,Userid,Productid,Price) VALUES (?,?,?,?)",item_name[0][0],session[:id],session[:produktid],item_price[0][0])
-    else
-        session[:add_to_cart_error] = 1
+        session[:results] = db.execute("SELECT Name,Description,Rating,Price,Productid FROM products WHERE Name = (?)",session[:search])
     end
-end
 
-def get_kundvagn
-    db = SQLite3::Database.new('db/shop.db')
-    db.results_as_hash = true
+    def thumbs_up
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
 
-    session[:items] = db.execute("SELECT Product,Userid,Productid,Price,Itemid FROM kundvagnar WHERE Userid = (?)",session[:id])
-end
+        rating = db.execute("SELECT Rating FROM products WHERE Productid = (?)",session[:produktid])
+        
+        new_rating = rating[0][0] + 1
 
-def buy_products
-    db = SQLite3::Database.new("db/shop.db")
-    db.results_as_hash = true
+        db.execute("UPDATE products SET Rating = (?) WHERE Productid = (?)",new_rating,session[:produktid])
+    end
 
-    db.execute("DELETE FROM kundvagnar WHERE Userid = ?", session[:id])
-end
+    def thumbs_down
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
 
-def delete_item
-    db = SQLite3::Database.new("db/shop.db")
-    db.results_as_hash = true
+        rating = db.execute("SELECT Rating FROM products WHERE Productid = (?)",session[:produktid])
+        
+        new_rating = rating[0][0] - 1
 
-    db.execute("DELETE FROM kundvagnar WHERE Itemid = ?", session[:itemid])
+        db.execute("UPDATE products SET Rating = (?) WHERE Productid = (?)",new_rating,session[:produktid])
+    end
+
+    def profiles
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
+
+        session[:profiles] = db.execute("SELECT Profiledescription,Item,Interest,Username FROM profiles WHERE Username = (?)",session[:username])
+    end
+
+    def create_comment
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
+
+        session[:comment] = params["comment"]
+
+        if session[:comment] == ""
+            session[:create_comment_error] = 1
+        elsif session[:id] == nil
+        else
+            db.execute("INSERT INTO comments (Comment,Commenter,Productid,Userid) VALUES (?,?,?,?)",session[:comment],session[:username],session[:produktid],session[:id])
+        end
+    end
+
+    def get_comments
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
+
+        session[:comments] = db.execute("SELECT Comment,Commenter,Productid,Userid,Commentid FROM comments WHERE Productid = (?)", session[:produktid])
+    end
+
+    def update_comment
+        db = SQLite3::Database.new("db/shop.db")
+        db.results_as_hash = true
+
+        session[:update_comment] = params["update_comment"]
+
+        if session[:update_comment] == ""
+            session[:update_comment_error] = 1
+        else
+            db.execute("UPDATE comments SET Comment = (?) WHERE Commentid = (?)",session[:update_comment],session[:comment_id])
+        end
+    end
+
+    def delete_comment
+        db = SQLite3::Database.new("db/shop.db")
+        db.results_as_hash = true
+
+        db.execute("DELETE FROM comments WHERE Commentid = ?", session[:comment_id])
+    end
+
+    def update_profile
+        db = SQLite3::Database.new("db/shop.db")
+        db.results_as_hash = true
+
+        session[:profile_description] = params["profile_description"]
+        session[:item] = params["item"]
+        session[:interest] = params["interest"]
+
+        db.execute("UPDATE profiles SET Profiledescription = ?, Item = ?, Interest = ? WHERE Username = ?",session[:profile_description],session[:item],session[:interest],session[:username])
+    end
+
+    def add_to_cart
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
+
+        item_name = db.execute("SELECT Name FROM products WHERE Productid = (?)",session[:produktid])
+        item_price = db.execute("SELECT Price FROM products WHERE Productid = (?)",session[:produktid])
+
+        if session[:id] != nil
+            db.execute("INSERT INTO kundvagnar (Product,Userid,Productid,Price) VALUES (?,?,?,?)",item_name[0][0],session[:id],session[:produktid],item_price[0][0])
+        else
+            session[:add_to_cart_error] = 1
+        end
+    end
+
+    def get_kundvagn
+        db = SQLite3::Database.new('db/shop.db')
+        db.results_as_hash = true
+
+        session[:items] = db.execute("SELECT Product,Userid,Productid,Price,Itemid FROM kundvagnar WHERE Userid = (?)",session[:id])
+    end
+
+    def buy_products
+        db = SQLite3::Database.new("db/shop.db")
+        db.results_as_hash = true
+
+        db.execute("DELETE FROM kundvagnar WHERE Userid = ?", session[:id])
+    end
+
+    def delete_item
+        db = SQLite3::Database.new("db/shop.db")
+        db.results_as_hash = true
+
+        db.execute("DELETE FROM kundvagnar WHERE Itemid = ?", session[:itemid])
+    end
 end
